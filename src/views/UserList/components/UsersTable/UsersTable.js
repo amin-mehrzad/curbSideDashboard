5670d92808191b4730d50f6ed23f37c775dfb898
Amin Mehrzad <amin.mehrzad@gmail.com>
2020-05-22 16:16:50 -0700

    CRUD created for product page

--- src/views/UserList/components/UsersTable/UsersTable.js
+++ src/views/UserList/components/UsersTable/UsersTable.js
@@ -1,23 +1,24 @@
-import React, { forwardRef, useState } from 'react';
+import React, { forwardRef, useState, useEffect } from 'react';
 import clsx from 'clsx';
 import PropTypes from 'prop-types';
 import moment from 'moment';
 import PerfectScrollbar from 'react-perfect-scrollbar';
 import { makeStyles } from '@material-ui/styles';
-import {
-  Card,
-  CardActions,
-  CardContent,
-  Avatar,
-  Checkbox,
-  Table,
-  TableBody,
-  TableCell,
-  TableHead,
-  TableRow,
-  Typography,
-  TablePagination
-} from '@material-ui/core';
+// import {
+//   Card,
+//   CardActions,
+//   CardContent,
+//   Avatar,
+//   Checkbox,
+//   Table,
+//   TableBody,
+//   TableCell,
+//   TableHead,
+//   TableRow,
+//   Typography,
+//   TablePagination
+// } from '@material-ui/core';
+import { Input } from '@material-ui/core';
 
 import { getInitials } from 'helpers';
 
@@ -39,185 +40,36 @@ import {
   Search,
   ViewColumn
 } from "@material-ui/icons";
-/*
-const useStyles = makeStyles(theme => ({
-  root: {},
-  content: {
-    padding: 0
-  },
-  inner: {
-    minWidth: 1050
-  },
-  nameContainer: {
-    display: 'flex',
-    alignItems: 'center'
-  },
-  avatar: {
-    marginRight: theme.spacing(2)
-  },
-  actions: {
-    justifyContent: 'flex-end'
-  }
-}));
 
-const UsersTable = props => {
-  const { className, users, ...rest } = props;
+import axios from 'axios';
+import setAuthToken from '../../../../utils/setAuthToken';
 
-  const classes = useStyles();
-
-  const [selectedUsers, setSelectedUsers] = useState([]);
-  const [rowsPerPage, setRowsPerPage] = useState(10);
-  const [page, setPage] = useState(0);
-
-  const handleSelectAll = event => {
-    const { users } = props;
-
-    let selectedUsers;
-
-    if (event.target.checked) {
-      selectedUsers = users.map(user => user.id);
-    } else {
-      selectedUsers = [];
-    }
-
-    setSelectedUsers(selectedUsers);
-  };
-
-  const handleSelectOne = (event, id) => {
-    const selectedIndex = selectedUsers.indexOf(id);
-    let newSelectedUsers = [];
-
-    if (selectedIndex === -1) {
-      newSelectedUsers = newSelectedUsers.concat(selectedUsers, id);
-    } else if (selectedIndex === 0) {
-      newSelectedUsers = newSelectedUsers.concat(selectedUsers.slice(1));
-    } else if (selectedIndex === selectedUsers.length - 1) {
-      newSelectedUsers = newSelectedUsers.concat(selectedUsers.slice(0, -1));
-    } else if (selectedIndex > 0) {
-      newSelectedUsers = newSelectedUsers.concat(
-        selectedUsers.slice(0, selectedIndex),
-        selectedUsers.slice(selectedIndex + 1)
-      );
-    }
-
-    setSelectedUsers(newSelectedUsers);
-  };
-
-  const handlePageChange = (event, page) => {
-    setPage(page);
-  };
-
-  const handleRowsPerPageChange = event => {
-    setRowsPerPage(event.target.value);
-  };
-
-  return (
-    <Card
-      {...rest}
-      className={clsx(classes.root, className)}
-    >
-      <CardContent className={classes.content}>
-        <PerfectScrollbar>
-          <div className={classes.inner}>
-            <Table>
-              <TableHead>
-                <TableRow>
-                  <TableCell padding="checkbox">
-                    <Checkbox
-                      checked={selectedUsers.length === users.length}
-                      color="primary"
-                      indeterminate={
-                        selectedUsers.length > 0 &&
-                        selectedUsers.length < users.length
-                      }
-                      onChange={handleSelectAll}
-                    />
-                  </TableCell>
-                  <TableCell>Name</TableCell>
-                  <TableCell>Email</TableCell>
-                  <TableCell>Location</TableCell>
-                  <TableCell>Phone</TableCell>
-                  <TableCell>Registration date</TableCell>
-                </TableRow>
-              </TableHead>
-              <TableBody>
-                {users.slice(0, rowsPerPage).map(user => (
-                  <TableRow
-                    className={classes.tableRow}
-                    hover
-                    key={user.id}
-                    selected={selectedUsers.indexOf(user.id) !== -1}
-                  >
-                    <TableCell padding="checkbox">
-                      <Checkbox
-                        checked={selectedUsers.indexOf(user.id) !== -1}
-                        color="primary"
-                        onChange={event => handleSelectOne(event, user.id)}
-                        value="true"
-                      />
-                    </TableCell>
-                    <TableCell>
-                      <div className={classes.nameContainer}>
-                        <Avatar
-                          className={classes.avatar}
-                          src={user.avatarUrl}
-                        >
-                          {getInitials(user.name)}
-                        </Avatar>
-                        <Typography variant="body1">{user.name}</Typography>
-                      </div>
-                    </TableCell>
-                    <TableCell>{user.email}</TableCell>
-                    <TableCell>
-                      {user.address.city}, {user.address.state},{' '}
-                      {user.address.country}
-                    </TableCell>
-                    <TableCell>{user.phone}</TableCell>
-                    <TableCell>
-                      {moment(user.createdAt).format('DD/MM/YYYY')}
-                    </TableCell>
-                  </TableRow>
-                ))}
-              </TableBody>
-            </Table>
-          </div>
-        </PerfectScrollbar>
-      </CardContent>
-      <CardActions className={classes.actions}>
-        <TablePagination
-          component="div"
-          count={users.length}
-          onChangePage={handlePageChange}
-          onChangeRowsPerPage={handleRowsPerPageChange}
-          page={page}
-          rowsPerPage={rowsPerPage}
-          rowsPerPageOptions={[5, 10, 25]}
-        />
-      </CardActions>
-    </Card>
-  );
-};
-
-UsersTable.propTypes = {
-  className: PropTypes.string,
-  users: PropTypes.array.isRequired
-};
-
-export default UsersTable;
-
-
-https://images.pexels.com/photos/461198/pexels-photo-461198.jpeg
-
-
-*/
 
 export default function MaterialTableDemo() {
+  // const getdata=()=>{
+  //   setAuthToken(`Bearer ${localStorage.jwtToken}`);
+  //   return axios.get(`${process.env.REACT_APP_BACKEND_URL}/API/products`)
+  //   .then(res => {
+  //     console.log('rate numbers-->', res.data.data);
+  //     console.log('data-->', res.data.data[0]);
+  //     const data = res.data.data;
+  //     //const total = res.data.data[0].metadata[0].total;
+  //     console.log(data)
+  //    // if(state.data)
+  //   //  setState({...state,data: data});
+  //   //  data;
+  //   //  return data
+  //   })
+  //   .catch(err => console.log(err))
+  // }
+
   const [state, setState] = React.useState({
     columns: [
       {
         field: 'url',
         title: 'Picture',
-        render: rowData => <img src={rowData.imageUrl} style={{ width: 30, height: 30, borderRadius: '20%' }} />
+        render: rowData => <img src={rowData.imageUrl} style={{ width: 30, height: 30, borderRadius: '20%' }} />,
+        editComponent: props => <input type="file" id="img" name="img" accept="image/*" />
       },
       { title: 'Name', field: 'name' },
       { title: 'Price', field: 'price', type: 'numeric' },
@@ -247,34 +99,18 @@ export default function MaterialTableDemo() {
         lookup: { 0: 'No', 1: 'Yes' },
       },
     ],
-    data: [
+    data: []
+  })
 
-      {
-        name: 'food1',
-        price: '9.99',
-        qty: 1,
-        tax: 1,
-        imageUrl: 'https://images.pexels.com/photos/461198/pexels-photo-461198.jpeg',
-        featured: 1,
-        barcode: 'dkf4444444dsd4',
-        category: 2,
-        active:1
+  // });
 
-      },
-      {
-        imageUrl: 'https://images.pexels.com/photos/461198/pexels-photo-461198.jpeg',
-        name: 'Zerya Betül',
-        price: '11.99',
-        qty: 2,
-        tax: 0,
-        featured: 0,
-        category: 0,
-        barcode: '3332hskj4sss564',
-        active:0
+  useEffect(async () => {
+    setAuthToken(`Bearer ${localStorage.jwtToken}`);
 
-      },
-    ],
-  });
+    const data = await axios.get(`${process.env.REACT_APP_BACKEND_URL}/API/products`)
+    console.log(data.data.data)
+    setState({ ...state, data: data.data.data });
+  }, []);
 
 
   const tableIcons = {
@@ -303,12 +139,16 @@ export default function MaterialTableDemo() {
       data={state.data}
       icons={tableIcons}
       editable={{
+
         onRowAdd: (newData) =>
+
           new Promise((resolve) => {
             setTimeout(() => {
               resolve();
               setState((prevState) => {
                 const data = [...prevState.data];
+                axios.post(`${process.env.REACT_APP_BACKEND_URL}/API/products`, newData)
+
                 data.push(newData);
                 return { ...prevState, data };
               });
@@ -319,8 +159,11 @@ export default function MaterialTableDemo() {
             setTimeout(() => {
               resolve();
               if (oldData) {
+
                 setState((prevState) => {
                   const data = [...prevState.data];
+
+                  axios.put(`${process.env.REACT_APP_BACKEND_URL}/API/products/${oldData._id}`, newData)
                   data[data.indexOf(oldData)] = newData;
                   return { ...prevState, data };
                 });
@@ -331,8 +174,11 @@ export default function MaterialTableDemo() {
           new Promise((resolve) => {
             setTimeout(() => {
               resolve();
+              console.log(oldData)
               setState((prevState) => {
                 const data = [...prevState.data];
+
+                axios.delete(`${process.env.REACT_APP_BACKEND_URL}/API/products/${oldData._id}`)
                 data.splice(data.indexOf(oldData), 1);
                 return { ...prevState, data };
               });
