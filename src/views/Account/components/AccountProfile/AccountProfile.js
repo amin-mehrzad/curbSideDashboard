0e18e601db4ee36f04f54ba48a6bcc4d4ef0d246
Amin Mehrzad <amin.mehrzad@gmail.com>
2020-06-01 14:38:25 -0700

    upload and edit logo finished

--- src/views/Account/components/AccountProfile/AccountProfile.js
+++ src/views/Account/components/AccountProfile/AccountProfile.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
 import clsx from 'clsx';
 import moment from 'moment';
@@ -13,6 +13,8 @@ import {
   Button,
   LinearProgress
 } from '@material-ui/core';
+import Axios from 'axios'
+import setAuthToken from '../../../../utils/setAuthToken'
 
 const useStyles = makeStyles(theme => ({
   root: {},
@@ -37,14 +39,77 @@ const useStyles = makeStyles(theme => ({
 const AccountProfile = props => {
   const { className, ...rest } = props;
 
+
+  const [profileProgress, setProfileProgress] = useState(100)
+
+  const [businessLogo, setBusinessLogo] = useState("")
   const classes = useStyles();
 
+  useEffect(() => {
+    async function fetchProfileInfo() {
+
+      setAuthToken(`Bearer ${localStorage.jwtToken}`);
+
+      const websiteInfo = await Axios.get(`${process.env.REACT_APP_BACKEND_URL}/API/websites`)
+      setBusinessLogo(websiteInfo.data)
+      console.log(websiteInfo)
+      var profileInformation = websiteInfo.data
+      var incompleteProps = 0
+      for (var profileProp in profileInformation) {
+        if (profileInformation[profileProp] === "")
+          incompleteProps += 10
+
+
+        console.log(incompleteProps)
+      }
+
+      setProfileProgress(100 - incompleteProps)
+
+    }
+    fetchProfileInfo()
+
+  }, [])
+
+  const handleUpload = event => {
+    event.preventDefault();
+    var tempFormData = new FormData()
+    console.log(event.target.files[0])
+
+    tempFormData.set('businessLogoUrl', event.target.files[0])
+
+
+    Axios.post(`${process.env.REACT_APP_BACKEND_URL}/API/websites`, tempFormData)
+      .then((response) => {
+        console.log(response)
+
+
+        setBusinessLogo(response.data)
+
+      })
+  }
+
+  const handleRemove = event => {
+    event.preventDefault();
+    var payload = businessLogo
+    payload={...payload,businessLogoUrl:""}
+
+    console.log(payload)
+    Axios.put(`${process.env.REACT_APP_BACKEND_URL}/API/websites`, payload)
+    .then((response) => {
+        console.log(response)
+        setBusinessLogo(response.data)
+  })
+
+}
+
+  console.log(businessLogo)
+
   const user = {
-    name: 'Shen Zhi',
+    name: `${businessLogo.businessName}`,
     city: 'Los Angeles',
     country: 'USA',
     timezone: 'GTM-7',
-    avatar: '/images/avatars/avatar_11.png'
+    avatar: `${process.env.REACT_APP_BACKEND_URL}/uploads/businessLogos/${businessLogo.businessLogoUrl}`
   };
 
   return (
@@ -59,7 +124,7 @@ const AccountProfile = props => {
               gutterBottom
               variant="h2"
             >
-              John Doe
+              {user.name}
             </Typography>
             <Typography
               className={classes.locationText}
@@ -82,23 +147,27 @@ const AccountProfile = props => {
           />
         </div>
         <div className={classes.progress}>
-          <Typography variant="body1">Profile Completeness: 70%</Typography>
+          <Typography variant="body1">Profile Completeness: {profileProgress}%</Typography>
           <LinearProgress
-            value={70}
+            value={profileProgress}
             variant="determinate"
           />
         </div>
       </CardContent>
       <Divider />
       <CardActions>
+        <input type="file" id="file" style={{ display: 'none' }} onChange={handleUpload} />
+
         <Button
           className={classes.uploadButton}
           color="primary"
           variant="text"
+          onClick={() => { document.getElementById("file").click() }}
         >
           Upload picture
         </Button>
-        <Button variant="text">Remove picture</Button>
+        <Button variant="text"
+          onClick={handleRemove}>Remove picture</Button>
       </CardActions>
     </Card>
   );
